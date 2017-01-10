3ca0a8c81e98744ff2b03029e361faf601a7b7d0
Engjell Rraklli <engjellrraklli@unnitech.net>
2017-01-10 15:21:30 +0100

    added default constructor for label with only one parameter the screen. With the addition of the relative layout concept and of the gravity, setting the position is obsolete and neccessary only for the absolute layout

--- src/tonegod/gui/controls/text/Label.java
+++ src/tonegod/gui/controls/text/Label.java
@@ -40,6 +40,19 @@ public class Label extends Element {
 	 * Creates a new instance of the Label control
 	 * 
 	 * @param screen The screen control the Element is to be added to
+	 * @param dimensions A Vector2f containing the width/height dimensions of the Element
+	 */
+	public Label(ElementManager screen) {
+		this(screen, UIDUtil.getUID(), Vector2f.ZERO, Vector2f.ZERO,
+			screen.getStyle("Label").getVector4f("resizeBorders"),
+			screen.getStyle("Label").getString("defaultImg")
+		);
+	}
+	
+	/**
+	 * Creates a new instance of the Label control
+	 * 
+	 * @param screen The screen control the Element is to be added to
 	 * @param position A Vector2f containing the x/y position of the Element
 	 * @param dimensions A Vector2f containing the width/height dimensions of the Element
 	 */
